4eefb7bfc6d68b9eee6ca824d449ab3cb44a13c6
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-06-03 19:59:57 +0530

    modify database.py

--- src/database.py
+++ src/database.py
@@ -1,6 +1,4 @@
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 db_url = "postgresql://postgres:_dhruv._.9@localhost:5432/superstore_dw"
 engine = create_engine(db_url)
-session = sessionmaker(autoflush= False, autocommit = False, bind=engine)
